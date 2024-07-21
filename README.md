7d4788568eba20e73324cd50959fe738fbb70cf6
WieszczY <wieszczy85@gmail.com>
2024-07-21 08:12:43 +0200

    Changing descriptions to English + organizing the code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# TFNC
-T.F.N.C. - Torrent File Name Cleaner
+# T.F.N.C. - Torrent File Name Cleaner
+
+## Description 
+
+<b>TFNC - Torrent File Name Cleaner</b> is a useful tool that automatically removes unnecessary additions from the names of files and folders downloaded via torrents. This program is particularly useful for users who want to maintain order in their multimedia directories, where excessive information in names can cause problems with organization or identification of content. TFNC can be configurable, allowing users to tailor the cleaning process to their own needs, ensuring that only unwanted parts of names are removed. Thanks to this tool, managing a file library becomes much easier and more efficient.
+
+## Dependencies
+
+Requires install **Java 21** to work!!!
--- src/main/java/pl/syntaxerr/FileRenamer.java
+++ src/main/java/pl/syntaxerr/FileRenamer.java
@@ -17,8 +17,6 @@ public class FileRenamer {
     private static final Logger LOGGER = Logger.getLogger(FileRenamer.class.getName());
     private final JTextField directoryField;
     private final JTextArea forbiddenWordsArea;
-    private final JButton directoryChooserButton;
-    private final JButton runButton;
     private final JFrame frame;
     private final File forbiddenWordsFile;
     private final File historyFile;
@@ -28,9 +26,9 @@ public class FileRenamer {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 300);
 
-        directoryField = new JTextField(20); // Ustawiamy szerokość pola tekstowego
-        directoryField.setToolTipText("Wpisz ścieżkę do katalogu tutaj lub wybierz za pomocą przycisku");
-        directoryChooserButton = new JButton("Wybierz katalog");
+        directoryField = new JTextField(20); // Set the width of the text field
+        directoryField.setToolTipText("Enter the directory path here or select using the button");
+        JButton directoryChooserButton = new JButton("Select directory");
 
         Dimension buttonSize = directoryChooserButton.getPreferredSize();
         directoryChooserButton.setPreferredSize(new Dimension(buttonSize.width / 2, buttonSize.height));
@@ -38,19 +36,19 @@ public class FileRenamer {
         forbiddenWordsArea = new JTextArea();
         forbiddenWordsArea.setLineWrap(true);
         forbiddenWordsArea.setWrapStyleWord(true);
-        forbiddenWordsArea.setToolTipText("Wpisz zakazane słowa tutaj, oddzielając je spacjami");
+        forbiddenWordsArea.setToolTipText("Enter forbidden words here, separating them with spaces");
 
-        runButton = new JButton("Uruchom");
+        JButton runButton = new JButton("Run");
 
         forbiddenWordsFile = new File("blacklist.txt");
         historyFile = new File("history.txt");
 
-        // Dodajemy przykładowe słowa do forbiddenWordsArea
+        // Add example words to forbiddenWordsArea
         forbiddenWordsArea.setText("[xtorrenty.org] [Ex-torrenty.org] [DEVIL-TORRENTS.PL] [POLSKIE-TORRENTY.EU] [superseed.byethost7.com] [Devil-Site.PL] [BEST-TORRENTS.ORG]");
 
         JPanel directoryPanel = new JPanel(new FlowLayout());
         directoryPanel.add(directoryField);
-        directoryPanel.add(Box.createVerticalStrut(10)); // Dodajemy pionowy odstęp
+        directoryPanel.add(Box.createVerticalStrut(10)); // Add vertical spacing
         directoryPanel.add(directoryChooserButton);
 
         runButton.addActionListener(e -> {
@@ -64,14 +62,14 @@ public class FileRenamer {
                     }
                 }
             } catch (IOException ex) {
-                LOGGER.severe("Wystapił błąd: " + ex.getMessage());
+                LOGGER.severe("An error occurred: " + ex.getMessage());
             }
         });
 
         directoryChooserButton.addActionListener(e -> {
             JFileChooser chooser = new JFileChooser();
             chooser.setCurrentDirectory(new File("."));
-            chooser.setDialogTitle("Wybierz katalog");
+            chooser.setDialogTitle("Select directory");
             chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
             chooser.setAcceptAllFileFilterUsed(false);
 
@@ -81,36 +79,33 @@ public class FileRenamer {
         });
 
         frame.setLayout(new BorderLayout());
-        frame.add(new JLabel("Katalog:"), BorderLayout.NORTH);
+        frame.add(new JLabel("Directory:"), BorderLayout.NORTH);
         frame.add(directoryPanel, BorderLayout.NORTH);
         frame.add(directoryChooserButton, BorderLayout.NORTH);
-        frame.add(new JLabel("Zakazane słowa:"), BorderLayout.CENTER);
+        frame.add(new JLabel("Forbidden words:"), BorderLayout.CENTER);
         frame.add(forbiddenWordsArea, BorderLayout.CENTER);
         frame.add(runButton, BorderLayout.SOUTH);
-
         frame.setVisible(true);
         frame.revalidate();
         frame.repaint();
     }
-
     public void renameFilesAndDirectoriesInDirectory(String directory, List<String> forbiddenWords) throws IOException {
-        // Zmiana nazw plików
+        // Rename files
         Path start = Paths.get(directory);
         try (Stream<Path> stream = Files.walk(start)) {
             stream.filter(Files::isRegularFile)
                     .forEach(path -> renameIfNecessary(path, forbiddenWords));
         }
-        LOGGER.info("Zmieniono nazwy plików...");
+        LOGGER.info("File names changed...");
 
-        // Zmiana nazw katalogów
+        // Rename directories
         try (Stream<Path> stream = Files.walk(start)) {
             stream.filter(Files::isDirectory)
                     .sorted(Comparator.comparing(Path::getNameCount).reversed())
                     .forEach(path -> renameIfNecessary(path, forbiddenWords));
         }
-        LOGGER.info("Zmieniono nazwy katalogów...");
+        LOGGER.info("Directory names changed...");
     }
-
     private void renameIfNecessary(Path path, List<String> forbiddenWords) {
         String name = path.getFileName().toString();
         for (String word : forbiddenWords) {
@@ -120,17 +115,16 @@ public class FileRenamer {
                 try {
                     Files.move(path, path.resolveSibling(newName));
                     try (PrintWriter writer = new PrintWriter(new FileWriter(historyFile, true))) {
-                        writer.println("Zmieniono nazwę pliku/katalogu: " + name + " na " + newName);
-                        LOGGER.severe("Zmieniono nazwę pliku/katalogu: " + name + " na " + newName);
+                        writer.println("Changed file/directory name: " + name + " to " + newName);
+                        LOGGER.severe("Changed file/directory name: " + name + " to " + newName);
                     }
                 } catch (IOException e) {
-                    LOGGER.severe("Wystapił błąd: " + e.getMessage());
+                    LOGGER.severe("An error occurred: " + e.getMessage());
                 }
                 break;
             }
         }
     }
-
     public static void main(String[] args)
     {
         new FileRenamer();
